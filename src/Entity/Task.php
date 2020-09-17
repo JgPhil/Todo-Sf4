ba9774ae2c3371d3b15f690f2713e1415f6a2d04
JgPhil <jamingph@gmail.com>
2020-09-17 21:03:52 +0200

    repositories added

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -6,8 +6,9 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
  * @ORM\Table
+ * 
  */
 class Task
 {
@@ -105,6 +106,18 @@ class Task
     public function setUser(?User $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getIsDone(): ?bool
+    {
+        return $this->isDone;
+    }
+
+    public function setIsDone(bool $isDone): self
+    {
+        $this->isDone = $isDone;
 
         return $this;
     }
