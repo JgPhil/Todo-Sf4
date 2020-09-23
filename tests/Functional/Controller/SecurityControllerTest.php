46e2bb42f5fb773db93322132ebffd03182a4803
JgPhil <jamingph@gmail.com>
2020-09-23 09:40:43 +0200

    initial commit with bootstrap form theme integrate

--- tests/Functional/Controller/SecurityControllerTest.php
+++ tests/Functional/Controller/SecurityControllerTest.php
@@ -8,8 +8,8 @@ class SecurityControllerTest extends AbstractWebTestCaseClass
 {
     public function testLogout()
     {
-        $this->logUtils->login(self::USERS[rand(0, 1)]); //login
-        $this->client->request('GET', '/'); //homepage
+        $this->logUtils->login(self::USERS[rand(0, 1)]); // first login
+        $this->client->request('GET', '/'); //homepage only accessible when logged
         $this->client->clickLink('Se déconnecter'); 
         $this->assertTrue($this->client->getResponse()->isRedirect()); 
         $this->client->followRedirect(); // login page
