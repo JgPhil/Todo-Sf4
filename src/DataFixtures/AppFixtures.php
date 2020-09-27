f955ab76bd28f286b5afe54c2194640f37a1b97f
JgPhil <jamingph@gmail.com>
2020-09-27 16:48:04 +0200

    fixtures  refactorized

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,52 +4,41 @@ namespace App\DataFixtures;
 
 use App\Entity\Task;
 use App\Entity\User;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Yaml\Yaml;
 
 class AppFixtures extends Fixture
 {
+
     public function load(ObjectManager $manager)
     {
+        $data = Yaml::parseFile('config/users.yaml')['users'];
 
+        foreach ($data as $userData) {
+            $user = new User;
+            $user->setUsername($userData["username"]);
+            $user->setPassword(password_hash($userData["password"], PASSWORD_BCRYPT));
+            $user->setEmail($userData["email"]);
+            $user->setRole($userData["role"]);
+            $manager->persist($user);
 
-        $user = new User;
-        $user->setUsername('user');
-        $user->setPassword(password_hash('user', PASSWORD_BCRYPT));
-        $user->setEmail('user@todo.fr');
-        $user->setRole('ROLE_USER');
-        $manager->persist($user);
-
-        $admin = new User;
-        $admin->setUsername('admin');
-        $admin->setPassword(password_hash('admin', PASSWORD_BCRYPT));
-        $admin->setEmail('admin@todo.fr');
-        $admin->setRole('ROLE_ADMIN');
-        $manager->persist($admin);
-
-        $anonym = new User;
-        $anonym->setUsername('anonym');
-        $anonym->setPassword(password_hash('anonym', PASSWORD_BCRYPT));
-        $anonym->setEmail('anonym@todo.fr');
-        $anonym->setRole('ROLE_USER');
-        $manager->persist($anonym);
-
-        $users =[$user, $admin];
-
-        for ($i = 0; $i < 55; $i++) {
-            $task = new Task;
-            $task->setUser($users[array_rand($users)]);
-            $task->setCreatedAt(new \DateTime());
-            $task->setIsDone(rand(0,1));
-            $task->setTitle('Titre de la tâche n° '. $i);
-            $task->setContent('Contenu de la tâche n° '.$i);
-            $manager->persist($task);
+            for ($i = 0; $i < 35; $i++) {
+                $task = new Task;
+                $task->setUser($user);
+                $task->setCreatedAt(new \DateTime());
+                $task->setIsDone(rand(0, 1));
+                $task->setTitle('Titre de la tâche n° ' . $i);
+                $task->setContent('Contenu de la tâche n° ' . $i);
+                $manager->persist($task);
+            }
+            $anonymousTasks = $manager->getRepository(Task::class)->findBy(['user' => null]);
+            foreach ($anonymousTasks as $anonymousTask) {
+                $anonymousTask->setUser(
+                    $manager->getRepository(User::class)->findBy(['username' => "anonym"])
+                );
+            }
+            $manager->flush();
         }
-         $anonymousTasks = $manager->getRepository(Task::class)->findBy(['user' => null]);
-        // fixture file loads the test database
-        foreach ($anonymousTasks as $anonymousTask) {
-            $anonymousTask->setUser($anonym);
-        } 
-        $manager->flush();
     }
 }
--- tests/Functional/Controller/TaskControllerTest.php
+++ tests/Functional/Controller/TaskControllerTest.php
@@ -40,51 +40,51 @@ class TaskControllerTest extends AbstractWebTestCaseClass
 
         if (!$crawler->filter('.thumbnail')->count()) {
             return;
-        } else { //fetching the first task elements
-            $firstTaskTitle = $crawler->filter('.task_update_link')->first()->text();
-            $firstTaskContent = $crawler->filter('.task_content')->first()->text();
-            $firstTaskShowLink = $crawler->filter('.task_update_link')->first()->attr('href');
-
-            $crawler = $this->client->request('GET', $firstTaskShowLink); //show
-            $editLink = $crawler->filter('.edit_task')->attr('href');
-            $crawler = $this->client->request('GET', $editLink);
-            $this->assertResponseIsSuccessful(); //edit
-
-            // Fetching the elements on the update form yo compare with the ones above
-            $titleTask = $crawler->filter('input[name="task[title]"]')->attr('value');
-            $contentTask = substr($crawler->filter('textarea[name="task[content]"]')->text(), 0, 30);
-
-            $this->assertEquals($firstTaskTitle, $titleTask);
-            $this->assertEquals($firstTaskContent, $contentTask);
         }
+        //fetching the first task elements
+        $firstTaskTitle = $crawler->filter('.task_update_link')->first()->text();
+        $firstTaskContent = $crawler->filter('.task_content')->first()->text();
+        $firstTaskShowLink = $crawler->filter('.task_update_link')->first()->attr('href');
+
+        $crawler = $this->client->request('GET', $firstTaskShowLink); //show
+        $editLink = $crawler->filter('.edit_task')->attr('href');
+        $crawler = $this->client->request('GET', $editLink);
+        $this->assertResponseIsSuccessful(); //edit
+
+        // Fetching the elements on the update form yo compare with the ones above
+        $titleTask = $crawler->filter('input[name="task[title]"]')->attr('value');
+        $contentTask = substr($crawler->filter('textarea[name="task[content]"]')->text(), 0, 30);
+
+        $this->assertEquals($firstTaskTitle, $titleTask);
+        $this->assertEquals($firstTaskContent, $contentTask);
     }
 
     public function testFormEditTask()
     {
         $this->logUtils->login('admin');
         $crawler = $this->client->request('GET', '/tasks');
+        
         if (!$crawler->filter('.thumbnail')->count()) {
             return;
-        } else {
-            // Fetch the first Task on the list page
-            $firstTaskShowLink = $crawler->filter('.task_update_link')->first()->attr('href');
-
-            $crawler = $this->client->request('GET', $firstTaskShowLink); //show
-            $editLink = $crawler->filter('.edit_task')->attr('href');
-            $crawler = $this->client->request('GET', $editLink);
-            $this->assertResponseIsSuccessful(); //edit
-
-            $updateTaskForm = $crawler->selectButton("Modifier")->form();
-            // Changing the form fields
-            $updateTaskForm['task[title]'] = 'tittre bidon';
-            $updateTaskForm['task[content]'] = 'Description bidon ';
-
-            $crawler = $this->client->submit($updateTaskForm);
-
-            $crawler = $this->client->followRedirect();
-
-            $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
         }
+        // Fetch the first Task on the list page
+        $firstTaskShowLink = $crawler->filter('.task_update_link')->first()->attr('href');
+
+        $crawler = $this->client->request('GET', $firstTaskShowLink); //show
+        $editLink = $crawler->filter('.edit_task')->attr('href');
+        $crawler = $this->client->request('GET', $editLink);
+        $this->assertResponseIsSuccessful(); //edit
+
+        $updateTaskForm = $crawler->selectButton("Modifier")->form();
+        // Changing the form fields
+        $updateTaskForm['task[title]'] = 'tittre bidon';
+        $updateTaskForm['task[content]'] = 'Description bidon ';
+
+        $crawler = $this->client->submit($updateTaskForm);
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
     }
 
 
@@ -92,13 +92,12 @@ class TaskControllerTest extends AbstractWebTestCaseClass
     {
         $this->logUtils->login('admin');
         $crawler = $this->client->request('GET', '/tasks');
+        $this->assertResponseIsSuccessful();
 
-        $user = $this->logUtils->getUser('admin')[1];
         $firstTask = $crawler->filter(".task")->first();
-        $firstTaskTitle = $crawler->filter(".task_update_link")->first()->text();
         $firstTaskId = explode('/', $firstTask->filter(".task_update_link")->attr('href'))[2];
 
-        $crawler = $this->client->request('DELETE',$firstTask->filter(".task_delete_btn")->attr('href'));
+        $crawler = $this->client->request('DELETE', $firstTask->filter(".task_delete_btn")->attr('href'));
 
         $crawler = $this->client->followRedirect();
 
